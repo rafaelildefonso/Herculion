8758b6274a8ccc41645993a78cf7dfd0fc12e528
Rafael Ildefonso <rafaelfurtado313@gmail.com>
2025-08-07 06:25:06 -0300

    add: smooth and etc

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+// create the scrollSmoother before your scrollTriggers
+ScrollSmoother.create({
+  smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+  effects: true, // looks for data-speed and data-lag attributes on elements
+  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
+
 // Sistema de scroll suave sem remover o scroll nativo
 class SmoothScroll {
   constructor() {
