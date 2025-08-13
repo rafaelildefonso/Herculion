1d841af06e31d993349849ef938c64c794cfd4f4
Rafael Ildefonso <rafaelfurtado313@gmail.com>
2025-08-12 22:15:37 -0300

    fix: header fixed

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 // create the scrollSmoother before your scrollTriggers
 ScrollSmoother.create({
+  content: ".main-content",
   smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
   effects: true, // looks for data-speed and data-lag attributes on elements
   smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+  onUpdate: (event) => {
+    document.querySelector(".header").classList.toggle("scrolled", event.progress > 0);
+  }
 });
 
 // Sistema de scroll suave sem remover o scroll nativo
